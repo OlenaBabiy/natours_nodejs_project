2a9daa86c97bc1c801b6054c1eedb69bfb38640d
olenkababiy04 <olenkababiy04@gmail.com>
2025-11-06 17:01:37 +0200

    Add use sign in

--- public/routes/userRouter.js
+++ public/routes/userRouter.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const authController = require('../../utils/authController');
 
 const router = express.Router();
 
-router
-  .route('/')
-  .get(userController.getAllUsers)
-  .post(userController.createUser);
+router.post('/signup', authController.signup);
+
+router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router
   .route('/api/v1/users')
   .get(userController.getUser)
